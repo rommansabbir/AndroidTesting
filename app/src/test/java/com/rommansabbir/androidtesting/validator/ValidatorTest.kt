b1c43060615ee3dee30adf345f7bc3826f537232
Romman Sabbir <rommansabbir@gmail.com>
2022-04-04 21:47:08 +0600

    ValidatorTest :: - Test commented

--- app/src/test/java/com/rommansabbir/androidtesting/validator/ValidatorTest.kt
+++ app/src/test/java/com/rommansabbir/androidtesting/validator/ValidatorTest.kt
@@ -1,18 +1,21 @@
 package com.rommansabbir.androidtesting.validator
 
 import org.junit.Test
+import org.junit.jupiter.api.assertThrows
 
 
 class ValidatorTest {
     private val validator = Validator()
+
+    /*Check if an given email address is valid*/
     @Test
     fun testValidEmailAddress() {
         assert(validator.isEmailValid("test@test.com"))
     }
 
+    /*Check if a given email address is invalid*/
     @Test
-    fun testInvalidEmail(){
+    fun testInvalidEmail() {
         assert(!validator.isEmailValid("test"))
     }
-
 }
