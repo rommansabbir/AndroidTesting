17ad414359299abdfdab5477c71566cb48006984
Romman Sabbir <rommansabbir@gmail.com>
2022-04-07 20:10:10 +0600

    MainViewModelTest :: - New test added to verify that the login functionality is invoked properly and the login is also successful

--- app/src/androidTest/java/com/rommansabbir/androidtesting/MainViewModelTest.kt
+++ app/src/androidTest/java/com/rommansabbir/androidtesting/MainViewModelTest.kt
@@ -2,14 +2,13 @@ package com.rommansabbir.androidtesting
 
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.viewModelScope
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.rommansabbir.androidtesting.data.AuthenticationRepository
 import com.rommansabbir.androidtesting.data.AuthenticationRepositoryImpl
+import com.rommansabbir.androidtesting.data.LoginModel
 import com.rommansabbir.androidtesting.data.LoginUseCase
 import com.rommansabbir.androidtesting.mock.MockDataProvider
-import com.rommansabbir.androidtesting.usecase.UseCase
 import junit.framework.TestCase
 import org.junit.Before
 import org.junit.Rule
@@ -39,18 +38,29 @@ internal class MainViewModelTest : TestCase() {
         viewModel = MainViewModel(dataSource, useCase)
     }
 
+    /*We are using UseCase, which calls repository behind the scene
+    and all operation is backed by coroutine*/
     @Test
-    fun verifyLogin() {
-        println("Msg: Verify Login")
-        viewModel.useCase(viewModel.viewModelScope, UseCase.None()) {
-            println("Msg: $it")
-            assert(it.isRight)
-        }
+    fun verifyLoginSuccessFromUseCase() {
+        var isInvoked = false
+        var isSuccess = false
+        viewModel.login(
+            LoginModel("admin", "1234567"),
+            {
+                isInvoked = true
+                isSuccess = true
+                println(it)
+            },
+            {
+                isInvoked = true
+                isSuccess = false
+                it.printStackTrace()
+            }
+        )
+        Thread.sleep(1000)
+        print("Is invoked : $isInvoked")
+        println("Is Success : $isSuccess")
+        assert(isInvoked)
+        assert(isSuccess)
     }
-
-    /*   @Test
-       fun getListOfData() {
-           viewModel.getMockData()
-           assert(viewModel.list.value?.find { it.msg == "Test" } == null)
-       }*/
 }
